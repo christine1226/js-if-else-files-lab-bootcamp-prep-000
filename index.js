a569883ec93bf69442c9cef7761438e693f552e3
christine.gomez1226@gmail.com <christine.gomez1226@gmail.com>
2018-07-14 21:32:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
-var name = "Joe"
-var height = "74"
+var name = "Susan"
+var height = "70"
+else if (height ==="70")
+console.log ("The height is correct")
+if (name ===Susan)
+console.log ("The name is correct")
 
 
 // Don't worry about this
 module.exports = { name, height
 }
-
